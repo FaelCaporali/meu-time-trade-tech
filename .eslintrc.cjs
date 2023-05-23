f2586ec46d50e9fafb7212cb5ce78f6a1ecee6e6
Fael Caporali <rafaelhon@gmail.com>
2023-05-23 10:17:32 -0300

    [feat - lint] improved delimeters rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,5 +10,19 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        "multiline": {
+          "delimiter": "semi",
+          "requireLast": true
+        },
+          "singleline": {
+            "delimiter": "semi",
+            "requireLast": false
+        }
+      },
+    ],
   },
 }
