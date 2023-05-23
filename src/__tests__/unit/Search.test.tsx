1c4feb0b2d62e6d50a61a3c682553604dc34b176
Fael Caporali <rafaelhon@gmail.com>
2023-05-23 16:26:40 -0300

    [refact - search] switched get to find: tests doesn't crash async improvement

--- src/__tests__/unit/Search.test.tsx
+++ src/__tests__/unit/Search.test.tsx
@@ -5,13 +5,13 @@ import userEvent from '@testing-library/user-event';
 
 describe('Testes da tela de busca', () => {
     describe('Elementos devem estar visíveis na tela', () => {
-        test('Busca elementos de filtro e botão de busca', () => {
+        test('Busca elementos de filtro e botão de busca', async () => {
             renderWithRouterAndStore(<Search />);
-            const country = screen.getByLabelText('Selecione o país');
-            const season = screen.getByLabelText('Selecione a temporada');
-            const league = screen.getByLabelText('Selecione o campeonato');
-            const team = screen.getByLabelText('Selecione o time');
-            const btn = screen.getByRole('button');
+            const country = await screen.findByLabelText('Selecione o país');
+            const season = await screen.findByLabelText('Selecione a temporada');
+            const league = await screen.findByLabelText('Selecione o campeonato');
+            const team = await screen.findByLabelText('Selecione o time');
+            const btn = await screen.findByRole('button');
 
             expect(country).toBeInTheDocument();
             expect(season).toBeInTheDocument();
@@ -22,35 +22,35 @@ describe('Testes da tela de busca', () => {
 
         test('Testa se elementos se tornam interativos em ordem correta', async () => {
             const wrapper = renderWithRouterAndStore(<Search />);
-            const country = screen.getByLabelText('Selecione o país');
-            const season = screen.getByLabelText('Selecione a temporada');
-            const league = screen.getByLabelText('Selecione o campeonato');
-            const team = screen.getByLabelText('Selecione o time');
-            const btn = screen.getByRole('button');
+            const country = await screen.findByLabelText('Selecione o país');
+            const season = await screen.findByLabelText('Selecione a temporada');
+            const league = await screen.findByLabelText('Selecione o campeonato');
+            const team = await screen.findByLabelText('Selecione o time');
+            const btn = await screen.findByRole('button');
 
             expect(country).not.toBeDisabled();
             expect(season).toBeDisabled();
 
-            userEvent.selectOptions(country, 'Brazil');
+            await userEvent.selectOptions(country, 'Brazil');
 
             expect(season).not.toBeDisabled();
             expect(league).toBeDisabled();
 
-            userEvent.selectOptions(season, '2022');
+            await userEvent.selectOptions(season, '2022');
             
             expect(league).not.toBeDisabled();
             expect(team).toBeDisabled();
             
-            userEvent.selectOptions(league, 'Serie A');
+            await userEvent.selectOptions(league, 'Serie A');
             
             expect(team).not.toBeDisabled();
             expect(btn).toBeDisabled();
 
-            userEvent.selectOptions(team, 'Atlético Mineiro');
+            await userEvent.selectOptions(team, 'Atlético Mineiro');
 
             expect(btn).not.toBeDisabled();
 
-            userEvent.click(btn);
+            await userEvent.click(btn);
 
             expect(wrapper.route).toBe('/results');
         })
