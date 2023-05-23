25e1d12610f2a2953c4d6a0f611a338c21719b83
Fael Caporali <rafaelhon@gmail.com>
2023-05-23 16:23:15 -0300

    [feat - search] initialized view to check test

--- src/views/Search.tsx
+++ src/views/Search.tsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
+import { Container, FormLabel, FormSelect } from "react-bootstrap";
+// import { useStore } from "react-redux";
+
 function Search() {
+  // const store = useStore();
+  const [country, setCountry] = useState('');
+
   return (
-    <div>Search</div>
+    <Container>
+      <FormLabel>Selecione o país
+        <FormSelect value={country} onChange={(e) => setCountry(e.target.value)}>
+          {/* {store.getState()
+          .countries.map((country) => (
+            <option key={country.name} value={country.name}>{country.name}</option>
+          ))
+          } */}
+        </FormSelect>
+      </FormLabel>
+    </Container>
   );
 }
 
