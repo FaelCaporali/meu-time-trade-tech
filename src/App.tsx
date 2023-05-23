2b843ddaf2c327fdf5ca98d1134742dfba82ce72
Fael Caporali <rafaelhon@gmail.com>
2023-05-23 12:21:34 -0300

    [fix - app] removed code of raw testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import { BrowserRouter } from 'react-router-dom';
-import { Button, Container, Navbar } from 'react-bootstrap';
+import { Container, Navbar } from 'react-bootstrap';
 import Routes from './components/App.Routes';
-import { useState } from 'react';
-import fetcher from './api/fetcher';
-import footballConfig from './api/football/config';
 // import { Link } from 'react-router-dom'
 
 function App() {
-  const [status, setStatus] = useState({});
-  
-  const fetchStatus = async () => {
-    try {
-      const response = await fetcher(footballConfig())
-      return response;
-    } catch (e) {
-      if (e instanceof Error) {
-        alert(e.message);
-      }
-    }
-  }
 
   return (
     <BrowserRouter basename={import.meta.env.DEV ? '/' : import.meta.env.BASE_URL}>
@@ -29,10 +14,6 @@ function App() {
         </Navbar>
         <Container>
           <Routes />
-        </Container>
-        <Container>
-          <Button onClick={async () => setStatus(await fetchStatus())}>teste</Button>
-          {Object.entries(status).map(([key, value]) => <p>{key}: {value as string}</p>)}
         </Container>
       </Container>
     </BrowserRouter>
