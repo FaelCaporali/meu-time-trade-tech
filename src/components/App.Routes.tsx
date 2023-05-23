a3ab760b51af87c9bd7ecb3dc8eef5870897293d
Fael Caporali <rafaelhon@gmail.com>
2023-05-23 09:17:35 -0300

    [feat - routes] added Routes component

---
+++ src/components/App.Routes.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+import LoginLogout from '../views/Auth'
+import Search from '../views/Search'
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/login" element={<LoginLogout />} />
+            <Route path="/search" element={<Search />} />
+        </Routes>
+    )
+}
+
+export default AppRoutes
