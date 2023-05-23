ee436b3eeabba6aecd4c06d8aab15f0de1b29807
Fael Caporali <rafaelhon@gmail.com>
2023-05-23 11:58:42 -0300

    [fix - env] removed dotenv references

--- src/api/football/config.ts
+++ src/api/football/config.ts
@@ -1,8 +1,4 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const devKey = process.env.API_SPORTS_KEY || '';
+const devKey = import.meta.env.DEV ? import.meta.env.VITE_API_SPORTS_KEY : '';
 const basicEndpoint = 'status';
 
 export default function footballConfig (endpoint: string = basicEndpoint, key: string = devKey) {
