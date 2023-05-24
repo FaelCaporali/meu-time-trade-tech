03479017b5c72440d327f2c4b5afbd702b77f72a
Fael Caporali <rafaelhon@gmail.com>
2023-05-24 20:27:19 -0300

    [feat - SelectInput] implemented logic to handle component and global state

--- src/resources/components/SelectInput.tsx
+++ src/resources/components/SelectInput.tsx
@@ -1,30 +1,36 @@
-import React from 'react'
-import { TSelectInputProps } from '../types/components/SelectInput'
+import React, { useEffect } from 'react'
 import { Container, FormLabel, FormSelect } from 'react-bootstrap'
-import translator from '../helpers/translator.json'
+import translator from '../../helpers/translator.json'
+import { TSelectInputProps } from '../../types/components/SelectInput';
 
 function SelectInput(props: TSelectInputProps) {
-  const [value, setValue] = React.useState<undefined | string>(undefined);
+  const [value, setValue] = React.useState<undefined | string | number>(undefined);
+
+  useEffect(() => {
+    props.onChange(value);
+  }, [value]);
+
 
   return (
     <Container>
-        <FormLabel htmlFor={props.field}>
-            Selecione o {translator.inputFields[props.field]}:
-            <FormSelect
-                value={value}
-                onChange={(e) => setValue(e.target.value)}
-                id={`select-${props.field}`}
-                disabled={props.disabled}
-            >
-                {
-                    props.options.map((option) => (
-                        <option key={`option-${props.field}-${option}`} value={option}>
-                            {option}
-                        </option>
-                    ))
-                }
-            </FormSelect>
-        </FormLabel>
+      <FormLabel htmlFor={`select-${props.field}`}>
+            Selecione {translator.inputFields[props.field]}
+        <FormSelect
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          id={`select-${props.field}`}
+          disabled={props.disabled}
+          name={props.field}
+        >
+          {
+            props.options.map((option) => (
+              <option key={`option-${props.field}-${option}`} value={option}>
+                {option}
+              </option>
+            ))
+          }
+        </FormSelect>
+      </FormLabel>
     </Container>
   )
 }
