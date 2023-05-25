68deb0efbd0abc7ce3467e1c1fa16f6bd7926061
Fael Caporali <rafaelhon@gmail.com>
2023-05-25 13:55:03 -0300

    [feat - players] Players table implemented

---
+++ src/resources/components/PlayersTable.tsx
@@ -0,0 +1,32 @@
+import { Table } from 'react-bootstrap'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../store';
+
+const PlayersTable = () => {
+  const { team } = useSelector((state: RootState) => state.filters);
+  const stats = useSelector((state: RootState) => state.teams.stats).find(t => t.name === team);
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Nome</th>
+          <th>Idade</th>
+          <th>Nacionalidade</th>
+        </tr>
+      </thead>
+      <tbody>
+        {stats?.players.map((p, i) => (
+          <tr>
+            <td>{i + 1}</td>
+            <td>{p.name}</td>
+            <td>{p.age}</td>
+            <td>{p.nationality}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  )
+}
+
+export default PlayersTable
