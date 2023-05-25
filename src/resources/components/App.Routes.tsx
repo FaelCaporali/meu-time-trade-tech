b8884f9b79225b1e9ea04bc0087a7ea84fd11bae
Fael Caporali <rafaelhon@gmail.com>
2023-05-25 13:54:41 -0300

    [feat + refact] added results route & '/' route

--- src/resources/components/App.Routes.tsx
+++ src/resources/components/App.Routes.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import LoginLogout from '../views/Auth';
+import StateInflator from './StateInflator';
 import Search from '../views/Search';
+import Results from '../views/Results';
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/login" element={<LoginLogout />} />
-      <Route path="/search" element={<Search />} />
+      <Route path='/' element={ <LoginLogout /> } />
+      <Route path="/search" element={<StateInflator><Search /></StateInflator>} />
+      <Route path="/results" element={<Results />} />
     </Routes>
   );
 };
