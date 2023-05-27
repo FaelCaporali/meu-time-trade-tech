69b24b34be54f3e2845adf7cca7ba6501f5c75e3
Fael Caporali <rafaelhon@gmail.com>
2023-05-26 22:53:00 -0300

    [feat + refact - Results] improved fetching logics

--- src/resources/views/Results.tsx
+++ src/resources/views/Results.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../store';
 import { fetchTeamDetails } from '../../api';
@@ -7,44 +7,91 @@ import { useNavigate } from 'react-router-dom';
 import PlayersTable from '../components/PlayersTable';
 import TeamScores from '../components/TeamScores';
 import GoalsChart from '../components/GoalsChart';
+import { ITeamStats } from '../../store/slices/teams';
+import GlobalLoading from '../components/GlobalLoading';
+import ComponentLoading from '../components/ComponentLoading';
 
 function Results() {
-  const filters = useSelector((state: RootState) => state.filters);
+  const [loading, setLoading] = useState(true);
+  const [canFetch, setFetch] = useState(false);
+  
   const userKey = useSelector((state: RootState) => state.user.key);
-  const statistics = useSelector((state: RootState) => state.teams.stats.find(t => t.name === filters.team));
+  const filters = useSelector((state: RootState) => state.filters);
+  const stats = useSelector((state: RootState) => state.teams.stats.find((t) => (
+    t.name == filters.team
+    && t.season == filters.season
+  )));
 
+  const [mostUsedLineUp, setLineUp] = useState<{ formation: string; played: number }[]>([]);
+  
   const redirect = useNavigate();
+  
+  useEffect(() => {
+    
+    if (!userKey || userKey.length === 0) redirect('/');
+    
+    else if (!stats) setFetch(true);
+    
+    setLoading(false);
+  }, []);
 
   useEffect(() => {
-    if (!userKey || !userKey.length) redirect('/');
-  }, [userKey]);
-
+    setLoading(true);
+    if (canFetch) {
+      const doTheFetch = async () => { await fetchTeamDetails() };
+      doTheFetch().then(() => {
+        setFetch(false);
+      });
+    }
+    setLoading(false);
+  }, [canFetch]);
+  
   useEffect(() => {
-    if (!statistics) fetchTeamDetails();
-  }, [filters, statistics]);
+    setLoading(true);
+    if (stats && stats.lineUp && stats.lineUp[0].formation != '') {
+      setLineUp(stats.lineUp.reduce(
+        (most: { formation: string; played: number }[], lineup: { formation: string; played: number }) => {
+          if (most[0].played < lineup.played) {
+            return [lineup];
+          }
+          if (most[0].played === lineup.played) {
+            most.push(lineup);
+            return most;
+          }
+          return most;
+        }, [{ formation: '', played: 0 }]
+      ));
+    }
+    setLoading(false);
+  }, [stats])
   
   return (
     <>
-      <h2>{filters.team}</h2>
-      <Container>
-        <p>{filters.league} - {filters.season}</p>
-      </Container>
-      <Container>
-        <h3>JOGADORES:</h3>
-        <PlayersTable />
-      </Container>
-      <Container>
-        <h3>FORMAÇÃO(ÕES) MAIS UTILIZADA(S):</h3>
-        <p>{statistics?.lineUp.map(f => f.formation)}</p>
-        <p>utilizada {statistics?.lineUp[0].played} vezes</p>
-      </Container>
-      <Container>
-        <h3>RESULTADOS:</h3>
-        <TeamScores />
-      </Container>
-      {/* <Container>
-        <GoalsChart />
-      </Container> */}
+      {
+        loading ? <GlobalLoading /> : 
+          <>
+            <h2>{filters.team}</h2>
+            <Container>
+              <p>{filters.league} - {filters.season}</p>
+            </Container>
+            {stats && stats.players ? (<Container>
+              <h3>JOGADORES:</h3>
+              <PlayersTable stats={stats}/>
+            </Container>) : <ComponentLoading />}
+            {!mostUsedLineUp ? <ComponentLoading /> : (<Container>
+              <h3>FORMAÇÃO(ÕES) MAIS UTILIZADA(S):</h3>
+              <ul>{mostUsedLineUp.map(f => <li key={`most-used-lineup-${f.formation}`}>{f.formation}</li>)}</ul>
+              <p>Utilizada(s) {mostUsedLineUp[0].played} vezes</p>
+            </Container>)}
+            {(stats && stats.fixtures) ? (<Container>
+              <h3>RESULTADOS:</h3>
+              <TeamScores stats={stats}/>
+            </Container>) : <ComponentLoading />}
+            {(stats && stats.goals) ? <Container>
+              <GoalsChart stats={stats} />
+            </Container> :  <ComponentLoading />}
+          </>
+      }
     </>
   );
 }
