5dc4fa0e8c7b72632c9ac6ea700d225dee7a8058
Fael Caporali <rafaelhon@gmail.com>
2023-05-30 09:05:28 -0300

    [feat - ci] testing jest reporter logs

--- jest.config.ts
+++ jest.config.ts
@@ -1,20 +1,19 @@
 import type { Config } from 'jest'
 
 const config: Config = {
-    transform: {
-        "^.+\\.tsx?$": "ts-jest",
-    },
-    testEnvironment: 'jsdom',
-    verbose: true,
-    preset: 'ts-jest',
-    moduleNameMapper: {
-        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/__tests__/__mocks__/fileMock.js',
-        '^/(.*)$': '<rootDir>/src/$1',
-        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    },
-    testMatch: ['**/__tests__/**/*.test.(ts|tsx)'],
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-    
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testEnvironment: 'jsdom',
+  verbose: true,
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/__tests__/__mocks__/fileMock.js',
+    '^/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
+  testMatch: ['**/__tests__/**/*.test.(ts|tsx)'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
 }
 
 export default config
